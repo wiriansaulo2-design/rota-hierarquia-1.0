b9ee2fccd3e6cf03ce864faae58b0ff83ed10205
wiriansaulo2-design <wiriansaulo2@gmail.com>
2026-02-21 05:51:22 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,14 @@
+const jwt = require("jsonwebtoken")
 
+module.exports = function(req, res, next) {
+  const token = req.header("Authorization")
+  if (!token) return res.status(401).json({ msg: "Sem token" })
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = decoded
+    next()
+  } catch {
+    res.status(401).json({ msg: "Token inválido" })
+  }
+}
