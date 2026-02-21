678f32a521b4d4897b59a7f9da0ea6b807c1817c
wiriansaulo2-design <wiriansaulo2@gmail.com>
2026-02-21 20:45:04 -0300

    Create hierarquia.js

---
+++ hierarquia.js
@@ -0,0 +1,35 @@
+const ordemPatentes=[
+"General",
+"Coronel",
+"Major",
+"Capitão",
+"Sargento",
+"Cabo",
+"Soldado"
+];
+
+function carregarHierarquia(){
+const div = document.getElementById('listaHierarquia');
+
+ db.collection("users").onSnapshot(snapshot=>{
+ div.innerHTML="";
+
+ ordemPatentes.forEach(p=>{
+
+ const grupo = document.createElement('div');
+ grupo.className='card';
+ grupo.innerHTML=`<h3>${p}</h3>`;
+
+ snapshot.forEach(doc=>{
+ const u=doc.data();
+ if(u.patente===p){
+ grupo.innerHTML += `<p>${u.nome}</p>`;
+ }
+ });
+
+ div.appendChild(grupo);
+ });
+ });
+}
+
+carregarHierarquia();
