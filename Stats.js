4c0b7ad074850aa707abb487548c367fa8291ff1
wiriansaulo2-design <wiriansaulo2@gmail.com>
2026-02-21 05:50:45 -0300

    Update Stats.js

--- Stats.js
+++ Stats.js
@@ -1,1 +1,14 @@
+const mongoose = require("mongoose")
 
+const StatsSchema = new mongoose.Schema({
+  ocorrencias: Number,
+  prisoes: Number,
+  armas: Number,
+  municoes: Number,
+  neutralizados: Number,
+  veiculos: Number,
+  dinheiro: Number,
+  drogas: Number
+})
+
+module.exports = mongoose.model("Stats", StatsSchema)
