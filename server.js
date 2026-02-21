08e89dcd5fa3be7a5d3e9181ff1788bb537344c0
wiriansaulo2-design <wiriansaulo2@gmail.com>
2026-02-21 05:49:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
 
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB conectado"))
+
+app.use("/api/auth", require("./routes/auth"))
+app.use("/api/stats", require("./routes/stats"))
+
+app.listen(5000, () => console.log("Servidor rodando na porta 5000"))
