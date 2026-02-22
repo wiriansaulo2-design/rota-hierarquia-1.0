7fd28426d7890113b4b837fcdf3e0abb0d1044f9
wiriansaulo2-design <wiriansaulo2@gmail.com>
2026-02-21 21:14:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ function mudarAba(id, botao) {
 
 window.onload = function() {
   setTimeout(function(){
-    document.getElementById("loading").style.display = "none";
-  }, 2000);
+    document.getElementById("loading").style.opacity = "0";
+    setTimeout(() => {
+      document.getElementById("loading").style.display = "none";
+    }, 500);
+  }, 2500);
 };
